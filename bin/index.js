43b81a189894b3d52b86111e1df4195aeba71554
Leo Picado <leopic@gmail.com>
2018-08-12 11:10:01 -0600

    Breaking CLI and Business logic apart

--- bin/index.js
+++ bin/index.js
@@ -1,4 +1,33 @@
 #!/usr/bin/env node
 
-var lib = require('../lib/index.js');
-    lib();
+// Module dependencies
+const argv = require('yargs').argv;
+
+const lib = require('../lib/index.js'),
+    ERROR_CODE = require('../lib/errors');
+
+// CLI Arguments
+const args = {
+    helpFlag: argv['h'] || false,
+    iconPath: argv['base-icon'] || ''
+};
+
+lib(args)
+    .then(console.log)
+    .catch(reason => {
+        switch (reason) {
+            case ERROR_CODE.WRONG_PATH:
+                console.error('Please provide a base icon using the --base-icon option');
+                break;
+            case ERROR_CODE.UNABLE_TO_CREATE_DIRECTORY:
+                console.error('Unable to create target directory');
+                break;
+            case ERROR_CODE.UNABLE_TO_OPEN_FILE:
+                console.error('Unable to open base icon file');
+                break;
+            default:
+                console.error('General error');
+        }
+
+        process.exit(1);
+    });
