a70c7dd15613cc3dc928d68442385b6f19b4848e
Leo Picado <leonardo.picado@zenjob.com>
2024-08-11 20:47:59 +0200

    simplified script

--- scripts/time-to-first-review.js
+++ scripts/time-to-first-review.js
@@ -64,34 +64,28 @@ const main = async () => {
       .filter((date) => date) // filter out null or undefined
       .sort()[0]; // Get the earliest review date
 
-    if (firstReviewDate) {
-      const timeToFirstReview = calculateTimeDifference(
-        prCreatedAt,
-        firstReviewDate
-      );
-      timesToFirstReview.push(timeToFirstReview);
-      console.log(
-        `PR #${pr.number} - Time to first review: ${timeToFirstReview} hours`
-      );
-    } else {
+    if (!firstReviewDate) {
       console.log(`PR #${pr.number} - No reviews found`);
+      return;
     }
+
+    const timeToFirstReview = calculateTimeDifference(
+      prCreatedAt,
+      firstReviewDate
+    );
+
+    timesToFirstReview.push(timeToFirstReview);
+
+    console.log(
+      `PR #${pr.number} - Time to first review: ${timeToFirstReview} hours`
+    );
   }
 
   // Calculate average and median
   if (timesToFirstReview.length > 0) {
     const total = timesToFirstReview.reduce((sum, value) => sum + value, 0);
     const average = total / timesToFirstReview.length;
-
-    const sortedTimes = timesToFirstReview.slice().sort((a, b) => a - b);
-    const middle = Math.floor(sortedTimes.length / 2);
-    const median =
-      sortedTimes.length % 2 === 0
-        ? (sortedTimes[middle - 1] + sortedTimes[middle]) / 2
-        : sortedTimes[middle];
-
     console.log(`Average time to first review: ${average.toFixed(2)} hours`);
-    console.log(`Median time to first review: ${median.toFixed(2)} hours`);
   } else {
     console.log(`No reviews found for the last ${NUM_PRS} PRs.`);
   }
