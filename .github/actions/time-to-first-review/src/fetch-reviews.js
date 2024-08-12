c05268afcc5c3c61ed4080e9b14f3881811ffc46
Leo Picado <leonardo.picado@zenjob.com>
2024-08-12 13:47:11 +0200

    one more?

--- .github/actions/time-to-first-review/src/fetch-reviews.js
+++ .github/actions/time-to-first-review/src/fetch-reviews.js
@@ -6,21 +6,15 @@ module.exports = async (repo, token, prNumber) => {
   try {
     const octokit = github.getOctokit(token);
 
-    const { data: reviews } = await octokit.rest.pulls.get({
+    const reviews = await octokit.rest.pulls.get({
       owner: repo.split("/")[0],
       repo: repo.split("/")[1],
       pull_number: prNumber,
     });
 
-    return reviews;
+    console.log(reviews);
 
-    // const response = await axios.get(
-    //   `https://api.github.com/repos/${repo}/pulls/${prNumber}/reviews`,
-    //   {
-    //     headers: { Authorization: `token ${token}` },
-    //   }
-    // );
-    // return response.data;
+    return reviews;
   } catch (error) {
     console.error(`Error fetching reviews for PR #${prNumber}:`, error.message);
     return [];
