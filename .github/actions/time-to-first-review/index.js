1b7abe8000f796de8b24d522e681c4a9578d010c
Leo Picado <leonardo.picado@zenjob.com>
2024-08-12 13:15:29 +0200

    simplify maybe

--- .github/actions/time-to-first-review/index.js
+++ .github/actions/time-to-first-review/index.js
@@ -4,45 +4,47 @@ const fetchPullRequests = require("./src/fetch-pull-requests");
 const fetchReviews = require("./src/fetch-reviews");
 const calculateTimeDifference = require("./src/calculate-time-difference");
 
+const getTimeToFirstReview = async (pr, repo, gitHubToken) => {
+  const prCreatedAt = pr.created_at;
+  const reviews = await fetchReviews(repo, gitHubToken, pr.number);
+
+  // Find the first review date
+  const firstReviewDate = reviews
+    .map((review) => review.submitted_at)
+    .filter((date) => date) // filter out null or undefined
+    .sort()[0]; // Get the earliest review date
+
+  if (!firstReviewDate) {
+    console.log(`PR #${pr.number} - No reviews found`);
+    return;
+  }
+
+  const timeToFirstReview = calculateTimeDifference(
+    prCreatedAt,
+    firstReviewDate
+  );
+
+  console.log(
+    `PR #${pr.number} - Time to first review: ${timeToFirstReview} hours`
+  );
+
+  return timeToFirstReview;
+};
+
 try {
   // GitHub repository information
   const repo = core.getInput("repo");
-  const gitHubToken = core.getInput("github_token");
-  const maxPRs = core.getInput("num_prs") || 500;
+  const token = core.getInput("github_token");
+  const maxPRs = core.getInput("num_prs") || 200;
 
   // Main function
   const main = async () => {
-    const prs = await fetchPullRequests(repo, gitHubToken, maxPRs);
-    let timesToFirstReview = [];
+    const prs = await fetchPullRequests(repo, token, maxPRs);
 
-    for (const pr of prs) {
-      const prCreatedAt = pr.created_at;
-      const reviews = await fetchReviews(repo, gitHubToken, pr.number);
+    let timesToFirstReview = await Promise.all(
+      prs.map((pr) => getTimeToFirstReview(pr, repo, token))
+    ).filter((time) => time);
 
-      // Find the first review date
-      const firstReviewDate = reviews
-        .map((review) => review.submitted_at)
-        .filter((date) => date) // filter out null or undefined
-        .sort()[0]; // Get the earliest review date
-
-      if (!firstReviewDate) {
-        console.log(`PR #${pr.number} - No reviews found`);
-        continue;
-      }
-
-      const timeToFirstReview = calculateTimeDifference(
-        prCreatedAt,
-        firstReviewDate
-      );
-
-      timesToFirstReview.push(timeToFirstReview);
-
-      console.log(
-        `PR #${pr.number} - Time to first review: ${timeToFirstReview} hours`
-      );
-    }
-
-    // Calculate average and median
     if (timesToFirstReview.length > 0) {
       const total = timesToFirstReview.reduce((sum, value) => sum + value, 0);
       const average = total / timesToFirstReview.length;
