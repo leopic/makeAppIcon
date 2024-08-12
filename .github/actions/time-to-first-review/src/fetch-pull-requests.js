373c62b910694c093408b247566cd99670c43bc8
Leo Picado <leonardo.picado@zenjob.com>
2024-08-12 13:38:01 +0200

    leveraging octokit

--- .github/actions/time-to-first-review/src/fetch-pull-requests.js
+++ .github/actions/time-to-first-review/src/fetch-pull-requests.js
@@ -1,14 +1,25 @@
 const axios = require("axios");
+const github = require("@actions/github");
 
 module.exports = async (repo, token, numPRs) => {
+  const octokit = github.getOctokit(token);
+
   try {
-    const response = await axios.get(
-      `https://api.github.com/repos/${repo}/pulls?state=closed&per_page=${numPRs}`,
-      {
-        headers: { Authorization: `token ${token}` },
-      }
-    );
-    return response.data;
+    const { data: pullRequests } = await octokit.rest.pulls.list({
+      owner: repo.split("/")[0],
+      repo: repo.split("/")[1],
+      state: "closed",
+      per_page: numPRs,
+    });
+
+    return pullRequests;
+    // const response = await axios.get(
+    //   `https://api.github.com/repos/${repo}/pulls?state=closed&per_page=${numPRs}`,
+    //   {
+    //     headers: { Authorization: `token ${token}` },
+    //   }
+    // );
+    // return response.data;
   } catch (error) {
     console.error("Error fetching pull requests:", error.message);
     return [];
