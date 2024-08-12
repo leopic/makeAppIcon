4bb23f102236c896113270009330f98a7d38bbc2
Leo Picado <leonardo.picado@zenjob.com>
2024-08-12 14:07:04 +0200

    back with axios

--- .github/actions/time-to-first-review/src/fetch-reviews.js
+++ .github/actions/time-to-first-review/src/fetch-reviews.js
@@ -1,26 +1,26 @@
 const axios = require("axios");
-const github = require("@actions/github");
+// const github = require("@actions/github");
 
 // Fetch reviews for a specific PR
 module.exports = async (repo, token, prNumber) => {
   try {
-    const octokit = github.getOctokit(token);
+    // const octokit = github.getOctokit(token);
 
-    const { data: reviews } = await octokit.pulls.listReviews({
-      owner: repo.split("/")[0],
-      repo: repo.split("/")[1],
-      pull_number: prNumber,
-    });
+    // const { data: reviews } = await octokit.pulls.listReviews({
+    //   owner: repo.split("/")[0],
+    //   repo: repo.split("/")[1],
+    //   pull_number: prNumber,
+    // });
 
-    // const response = await axios.get(
-    //   `https://api.github.com/repos/${repo}/pulls/${prNumber}/reviews`,
-    //   {
-    //     headers: { Authorization: `token ${token}` },
-    //   }
-    // );
-    // return response.data;
+    const response = await axios.get(
+      `https://api.github.com/repos/${repo}/pulls/${prNumber}/reviews`,
+      {
+        headers: { Authorization: `token ${token}` },
+      }
+    );
+    return response.data;
 
-    return reviews;
+    // return reviews;
   } catch (error) {
     console.error(`Error fetching reviews for PR #${prNumber}:`, error.message);
     return [];
