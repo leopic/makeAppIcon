5499c336543ccded74b5e06276ab4e4121484a92
Leo Picado <leonardo.picado@zenjob.com>
2024-08-12 13:54:43 +0200

    fetching the wrong endpoint, oops

--- .github/actions/time-to-first-review/src/fetch-reviews.js
+++ .github/actions/time-to-first-review/src/fetch-reviews.js
@@ -6,13 +6,28 @@ module.exports = async (repo, token, prNumber) => {
   try {
     const octokit = github.getOctokit(token);
 
-    const { data } = await octokit.rest.pulls.get({
+    // const { pullRequest } = await octokit.rest.pulls.get({
+    //   owner: repo.split("/")[0],
+    //   repo: repo.split("/")[1],
+    //   pull_number: prNumber,
+    // });
+
+    // console.log(pullRequest);
+
+    // Fetch pull request reviews
+    const { data: reviews } = await octokit.rest.pulls.listReviews({
       owner: repo.split("/")[0],
       repo: repo.split("/")[1],
       pull_number: prNumber,
     });
 
-    return data;
+    console.log(reviews);
+
+    return reviews;
+
+    // return {
+    //   pullRequest
+    // };
   } catch (error) {
     console.error(`Error fetching reviews for PR #${prNumber}:`, error.message);
     return [];
