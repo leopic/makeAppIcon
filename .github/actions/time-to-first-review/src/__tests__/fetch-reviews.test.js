f498c6b69b9db364eed268dacecd5bd8268ebcf2
Leo Picado <leonardo.picado@zenjob.com>
2024-08-13 14:02:10 +0200

    tests done for fetch-reviews

---
+++ .github/actions/time-to-first-review/src/__tests__/fetch-reviews.test.js
@@ -0,0 +1,59 @@
+const mockListReviews = jest.fn().mockReturnValue({
+  data: [
+    {
+      id: 1,
+      submitted_at: "2021-09-01T00:00:00Z",
+    },
+    {
+      id: 2,
+      submitted_at: "2021-09-02T00:00:00Z",
+    },
+  ],
+});
+
+jest.mock("@actions/github", () => ({
+  getOctokit: jest.fn().mockReturnValue({
+    rest: {
+      pulls: {
+        listReviews: mockListReviews,
+      },
+    },
+  }),
+}));
+
+const fetchReviews = require("../fetch-reviews");
+
+describe("fetchReviews", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("logs an error to the console when the request fails", async () => {
+    const mockError = new Error("Request failed");
+    mockListReviews.mockRejectedValueOnce(mockError);
+
+    const consoleErrorSpy = jest.spyOn(console, "error").mockImplementation();
+
+    const reviews = await fetchReviews("owner/repo", "token", 3);
+
+    expect(reviews.length).toBe(0);
+    expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
+    expect(consoleErrorSpy).toHaveBeenCalledWith(
+      "Error fetching reviews for PR #3:",
+      "Request failed"
+    );
+
+    consoleErrorSpy.mockRestore();
+  });
+  it("correctly parses and creates the request for reviews of a single PR", async () => {
+    const reviews = await fetchReviews("owner/repo", "token", 3);
+
+    expect(reviews.length).toBe(2);
+    expect(mockListReviews).toHaveBeenCalledTimes(1);
+    expect(mockListReviews).toHaveBeenCalledWith({
+      owner: "owner",
+      repo: "repo",
+      pull_number: 3,
+    });
+  });
+});
--- .github/actions/time-to-first-review/src/fetch-pull-requests.js
+++ .github/actions/time-to-first-review/src/fetch-pull-requests.js
@@ -3,6 +3,11 @@ const github = require("@actions/github");
 module.exports = async (repo, token, numPRs) => {
   const octokit = github.getOctokit(token);
 
+  if (numPRs > 100) {
+    numPRs = 100;
+    console.log("Number of PRs requested is greater than 100");
+  }
+
   try {
     const { data: pullRequests } = await octokit.rest.pulls.list({
       owner: repo.split("/")[0],
